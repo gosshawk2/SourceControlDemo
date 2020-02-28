8da05c1e50aecb1f2448ea89f9e35343ba51d3e1
Daniel Goss <gosshawk2@yahoo.co.uk>
2020-02-28 18:20:08 +0000

    SourceControlDemo v1.0.0.0 First Commit. MDI application - used default from vs2019 options. DG 28-02-2020 18:20

---
+++ SourceControlDemo/Class_Users.vb
@@ -0,0 +1,33 @@
+﻿Public Class clsUsers
+    Private _Firstname As String
+    Private _Lastname As String
+    Private _Username As String
+
+    Public Property Firstname As String
+        Get
+            Firstname = _Firstname
+        End Get
+        Set(value As String)
+            _Firstname = value
+        End Set
+    End Property
+
+    Public Property Lastname As String
+        Get
+            Lastname = _Lastname
+        End Get
+        Set(value As String)
+            _Lastname = value
+        End Set
+    End Property
+
+    Public Property Username As String
+        Get
+            Username = _Username
+        End Get
+        Set(value As String)
+            _Username = value
+        End Set
+    End Property
+
+End Class
